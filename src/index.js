cabcee44cd9ae83808dc483f44450f79854ba167
Barbara Oleszek <barbaraoleszek@gmail.com>
2021-05-08 00:01:49 +0200

    added information from API and fix to hours and minutes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ let getWind = document.querySelector("#get-wind");
 let getHumidity = document.querySelector("#get-humidity");
 let weatherDescription = document.querySelector("#weather-description");
 let mainIcon = document.querySelector("#main-icon");
-let daysForForecast = []
 
 
 // Date and time
@@ -13,8 +12,15 @@ let daysForForecast = []
 let now = new Date();
 let dayDate = document.querySelector("#day-date");
 let weekday = now.toLocaleString("default", { weekday: "long" });
-
-dayDate.innerHTML = `${weekday}, ${now.getHours()}:${now.getMinutes()}`;
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+dayDate.innerHTML = `${weekday}, ${hours}:${minutes}`;
 
 // On Load Location
 
@@ -36,6 +42,8 @@ function getTempInLocation(response) {
   mainIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png" style="width: 82px;"/>`;
   getWind.innerText = response.data.wind.speed;
  getHumidity.innerText = response.data.main.humidity;
+
+ getForecast(response.data.coord);
 }
 
   navigator.geolocation.getCurrentPosition(handlePosition);
@@ -73,27 +81,39 @@ function showDayInfo(response) {
   getWind.innerText = response.data.wind.speed;
   getHumidity.innerText = response.data.main.humidity;
 
- 
+ getForecast(response.data.coord);
 
 
 }
 // Forecast
+function getForecast(coords) {
+  axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&appid=${apiKey}&units=metric`).
+  then(displayForecast);
 
-function displayForecast() {
+}
+function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#weather-forecast");
   let forecastHTML = "";
-  forecastHTML = forecastHTML + `<div class="row p-2 justify-content-center first">
-        <div class="col p-2 col-sm-2 day">Monday</div>
-        <div class="col p-2 col-sm-2 icon"><img src="http://openweathermap.org/img/wn/50d@2x.png" style="width: 45px;"/></div>
-        <div class="col p-2 col-sm-2 temperature">12°/3°</div>
-      </div>`;
 
-      forecastElement.innerHTML = forecastHTML;
+  forecast.forEach(function (forecastDt, index) {
+if (index < 6 ) {
+  forecastHTML = forecastHTML + `<div class="row p-2 justify-content-center first">
+        <div class="col p-2 col-sm-2 day">${new Date(forecastDt.dt *1000).toLocaleString("default", { weekday: "long" })}</div>
+        <div class="col p-2 col-sm-2 icon"><img src="http://openweathermap.org/img/wn/${forecastDt.weather[0].icon}@2x.png" style="width: 45px;"/></div>
+        <div class="col p-2 col-sm-2 temperature">${Math.round(forecastDt.temp.max)}°/${Math.round(forecastDt.temp.min)}°</div>
+      </div>`;
+    }
+    
+  })
+  
+  forecastElement.innerHTML = forecastHTML;
+      
 }
 
 
 
-displayForecast();
+
 
 
 
